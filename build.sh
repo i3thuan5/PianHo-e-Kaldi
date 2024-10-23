7d779378c42bbcf8f39e158baaf8a4d8234433ba
Tia i X570-ACE <hong@ithuan.tw>
2024-10-23 16:06:00 +0800

    整理build指令，加--pull。

--- build.sh
+++ build.sh
@@ -2,8 +2,12 @@
 
 set -euxo pipefail
 
-docker build . -t ithuan/pianho-e-kaldi:debian-bookworm -f Dockerfile-debian
+docker build --pull \
+	-f Dockerfile-debian . \
+	-t ithuan/pianho-e-kaldi:debian-bookworm
 docker push ithuan/pianho-e-kaldi:debian-bookworm
 
-docker build . -t ithuan/pianho-e-kaldi:ubuntu-24.04 -f Dockerfile-ubuntu
+docker build --pull \
+	-f Dockerfile-ubuntu . \
+	-t ithuan/pianho-e-kaldi:ubuntu-24.04
 docker push ithuan/pianho-e-kaldi:ubuntu-24.04
