69d431857020c17d0c1cd30495ed0d7a04eb1fb7
Tia i X570-ACE <hong@ithuan.tw>
2024-10-23 16:09:11 +0800

    shellcheck

--- shellcheck.sh
+++ shellcheck.sh
@@ -5,5 +5,5 @@ do
     shellcheck --severity=info "$file";
     tsitkai="$?"
     exit_code=$(( tsitkai != 0 ? tsitkai : exit_code))
-done <   <(find . -type f -name '*.sh' -not -path './venv/*' -print0)
+done <   <(find . -type f -name '*.sh' -not -path './venv/*' -not -path './.git/*' -print0)
 exit $(( exit_code == 0 ? 0 : 1))
