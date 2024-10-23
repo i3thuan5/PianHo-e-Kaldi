ba491a1d155845dd609e83018146fb1f823c8f8b
Tia i X570-ACE <hong@ithuan.tw>
2024-10-23 16:04:13 +0800

    先做較有tih用ê debian版。

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-docker build . -t ithuan/pianho-e-kaldi:ubuntu-24.04 -f Dockerfile-ubuntu
-docker push ithuan/pianho-e-kaldi:ubuntu-24.04
+set -euxo pipefail
 
 docker build . -t ithuan/pianho-e-kaldi:debian-bookworm -f Dockerfile-debian
 docker push ithuan/pianho-e-kaldi:debian-bookworm
+
+docker build . -t ithuan/pianho-e-kaldi:ubuntu-24.04 -f Dockerfile-ubuntu
+docker push ithuan/pianho-e-kaldi:ubuntu-24.04
