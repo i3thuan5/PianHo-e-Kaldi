257a3b849eac6d7d221286ef205fa17b25e9a5a3
Tia i X570-ACE <hong@ithuan.tw>
2024-11-18 15:45:06 +0800

    build ê時陣攏檢查apt敢有新套件。

--- build.sh
+++ build.sh
@@ -3,16 +3,19 @@
 set -euxo pipefail
 
 docker build --pull \
+	--no-cache-filter base \
 	-f Dockerfile-debian . \
 	-t ithuan/pianho-e-kaldi:debian-bookworm
 docker push ithuan/pianho-e-kaldi:debian-bookworm
 
 docker build --pull \
+	--no-cache-filter base \
 	-f Dockerfile-ubuntu . \
 	-t ithuan/pianho-e-kaldi:ubuntu-24.04
 docker push ithuan/pianho-e-kaldi:ubuntu-24.04
 
 docker build --pull \
+	--no-cache-filter base \
 	-f Dockerfile-ubuntu22.04-cuda . \
 	-t ithuan/pianho-e-kaldi:ubuntu22.04-cuda
 docker push ithuan/pianho-e-kaldi:ubuntu22.04-cuda
